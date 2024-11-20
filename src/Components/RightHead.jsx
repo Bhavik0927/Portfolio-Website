c8afd57d73928b8d8dcc00fb1b9c4feb13232848
Bhavik <bhaviktembhare2@gmail.com>
2024-11-21 00:51:40 +0530

    done some basic changes

--- src/Components/RightHead.jsx
+++ src/Components/RightHead.jsx
@@ -8,10 +8,10 @@ const RightHead = () => {
     return (
 
         <div className={` h-36 w-full flex items-center justify-between  p-2 px-8 `}
-        style={{ backgroundImage: "url('/src/images/blur-background.jpg')" }}>
+        style={{ backgroundImage: "url('https://images.unsplash.com/photo-1464802686167-b939a6910659?q=80&w=2050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
             <div className="flex items-center justify-center gap-5">
                 <div className="w-24 h-24 rounded-full border-solid border-[3px] border-white overflow-hidden">
-                    <img className="w-full h-full" src="src/images/port.jpg" alt="port" />
+                    <img className="w-full h-full" src="https://i.postimg.cc/X7RHGVcN/1691651295602.jpg" alt="port" />
                 </div>
                 <div>
                     <h1 className="text-2xl font-bold text-white">Bhavik Arun Tembhare</h1>
--- src/Components/StackContainer.jsx
+++ src/Components/StackContainer.jsx
@@ -2,24 +2,27 @@ import Stacks from "../Stacks";
 import StackBlock from "./StackBlock";
 
 const StackContainer = () => {
-  return (
-    <div className=" p-10 flex gap-5 flex-wrap justify-center bg-[rgb(20,19,26)]">
-        <div className="p-4 mt-2">
-            <h1 className="text-white font-bold mb-1 text-lg">What I Know </h1>
-            <div className="flex flex-wrap  ">
-                {
-                    Stacks.map((data) =>(
-                        <StackBlock key={data.id} data={data} />
-                    ))
-                }
+    return (
+        <div className=" p-10 flex gap-5 flex-wrap justify-center bg-[rgb(20,19,26)]">
+            <div className="p-4 mt-2">
+                <h1 className="text-white font-bold mb-1 text-lg">What I Know </h1>
+                <div className="flex flex-wrap  ">
+                    {
+                        Stacks.map((data) => (
+                            <StackBlock key={data.id} data={data} />
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="w-52 bg-[rgb(28,27,35)] ">
+                <h2 className="text-white font-bold mb-1 mt-5 text-center text-lg ">More Projects</h2>
+                <div className="px-3">
+                    <button className="border-[1px] border-black text-[rgb(139,138,145)] bg-[rgb(28,27,35)] rounded-md  m-1 py-1 px-2 ">Bubble Game</button>
+                    <button className="border-[1px] border-indigo-700 text-[rgb(139,138,145)] bg-[rgb(28,27,35)] rounded-md  m-1 py-1 px-2 ">Short URL</button>
+                </div>
             </div>
         </div>
-        <div className="w-52 bg-[rgb(28,27,35)] ">
-            <h2 className="text-white font-bold mb-1 mt-5 text-center text-lg ">More Projects</h2>
-            
-        </div>
-    </div>
-  )
+    )
 }
 
 export default StackContainer
